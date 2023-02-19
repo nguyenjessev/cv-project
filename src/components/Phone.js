f63c51d940b8babc821ae3f469c52d29f6b013b1
Jesse Nguyen <nguyenjessev@gmail.com>
2023-02-19 14:32:01 -0500

    Add Phone info skeleton

--- src/components/Phone.js
+++ src/components/Phone.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class Phone extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      phone: '123-4567',
+      editing: false,
+    };
+  }
+
   render() {
     return (
       <div className='phone-container'>
         <h3>Phone</h3>
+        {this.state.phone}
       </div>
     );
   }
