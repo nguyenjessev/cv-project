4d51b0e617b6d9af9b0f2ce9554cde69d06a2690
Jesse Nguyen <nguyenjessev@gmail.com>
2023-02-25 14:47:20 -0500

    Style and add toggling between editing/not

--- src/components/EducationEntry.js
+++ src/components/EducationEntry.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/EducationEntry.css';
+import Button from './Button';
 
 class EducationEntry extends React.Component {
   constructor(props) {
@@ -13,14 +14,55 @@ class EducationEntry extends React.Component {
     };
   }
 
+  handleClickSave = () => {
+    this.setState({
+      editing: false,
+    });
+  };
+
+  handleClickEdit = () => {
+    this.setState({
+      editing: true,
+    });
+  };
+
   entry = () => {
-    return (
-      <div className='education-entry'>
-        <h3 className='school-name'>{this.state.schoolName}</h3>
-        <h4 className='major'>{this.state.major}</h4>
-        <p className='notes'>{this.state.notes}</p>
-      </div>
-    );
+    if (this.state.editing) {
+      return (
+        <div className='education-entry'>
+          <input
+            type='text'
+            name='school-name-input'
+            id='school-name-input'
+            value={this.state.schoolName}
+          />
+          <input
+            type='text'
+            name='major-input'
+            id='major-input'
+            value={this.state.major}
+          />
+          <textarea
+            type='text'
+            name='notes-input'
+            id='notes-input'
+            value={this.state.notes}
+          />
+          <Button name='Save' onClick={this.handleClickSave} />
+        </div>
+      );
+    } else {
+      return (
+        <div className='education-entry'>
+          <h3 className='school-name'>
+            {this.state.schoolName}
+            <Button name='Edit' onClick={this.handleClickEdit} />
+          </h3>
+          <h4 className='major'>{this.state.major}</h4>
+          <p className='notes'>{this.state.notes}</p>
+        </div>
+      );
+    }
   };
 
   render() {
